b4fd4b8d03ad6e59770fe2d9572883b5693d450b
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-25 00:38:10 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_gpt/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const ChatScreen(),
+    );
+  }
+}
