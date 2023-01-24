b084b52ab3884698f8e976f24f2d882706ef3a86
Smit Lakhani <smitlakhani13@gmail.com>
2023-01-25 01:14:33 +0530

    added user

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:chat_gpt/chatmessage.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -8,10 +12,62 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final List<ChatMessage> _messages = [];
+
+  void _sendMessage() {
+    ChatMessage _message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+    );
+
+    setState(() {
+      _messages.insert(0, _message);
+    });
+
+    _controller.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _controller,
+            onSubmitted: (value) => _sendMessage(),
+            decoration: InputDecoration.collapsed(hintText: "Send a message"),
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.send),
+          onPressed: () => _sendMessage(),
+        )
+      ],
+    ).px16();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('ChatGPT')),
+      appBar: AppBar(title: const Text("Smit's_ChatGPT_Bot")),
+      body: SafeArea(
+        child: Column(children: [
+          Flexible(
+            child: ListView.builder(
+              reverse: true,
+              padding: Vx.m8,
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return _messages[index];
+              },
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: context.cardColor),
+            child: _buildTextComposer(),
+          )
+        ]),
+      ),
     );
   }
 }
